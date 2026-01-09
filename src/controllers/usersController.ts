f0b8b387c6996f74e9902631a21708a6c565d73d
doimih <doimih@gmail.com>
2026-01-09 16:31:01 +0200

    Modificari parola

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,6 +1,8 @@
 import { z } from "zod";
+import type { Request, Response } from "express";
 import { CreateUserDto } from "../dto/user.dto";
 import { createUser, listUsers } from "../services/usersService";
+import { password } from "bun";
 
 export const getUsers = async () => {
   return await listUsers();
@@ -17,4 +19,13 @@ export const addUser = async (body: any) => {
   }
 
   return await createUser(parsed.data);
+};
+
+export const createUserController = async (req: Request, res: Response) => {
+  const user = await createUser({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+  res.status(201).json(user);
 };
--- src/dto/user.dto.ts
+++ src/dto/user.dto.ts
@@ -1,8 +1,9 @@
 import { z } from "zod";
 
 export const CreateUserDto = z.object({
-  name: z.string().min(2).max(50),
+  name: z.string().min(1),
   email: z.string().email(),
+  password: z.string().min(6),
 });
 
 export type CreateUserInput = z.infer<typeof CreateUserDto>;
